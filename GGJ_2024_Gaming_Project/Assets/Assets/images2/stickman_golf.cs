cf2fc381a1c19e3173a79cb8da50c77c38d97705
riolinabc <riolinabc@gmail.com>
2024-01-25 00:41:42 +0800

    Set stickman throw stick animation
    Set stickman throw stick animation

---
+++ GGJ_2024_Gaming_Project/Assets/Assets/images2/stickman_golf.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickman_golf : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetMouseButtonDown(0)){
+        this.animator.SetTrigger("ThrowTrigger");}
+    }
+}
