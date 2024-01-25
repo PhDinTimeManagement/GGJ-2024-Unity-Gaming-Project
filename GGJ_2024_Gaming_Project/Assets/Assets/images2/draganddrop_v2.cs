917a065d6f3fd49e57551e9493bc1ee40e8a025f
riolinabc <riolinabc@gmail.com>
2024-01-26 00:50:44 +0800

    DragAndDrop and Animation(only one play each click)

---
+++ GGJ_2024_Gaming_Project/Assets/Assets/images2/draganddrop_v2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class draganddrop_v2 : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = GetMouseWorldPosition() + offset;
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+}
